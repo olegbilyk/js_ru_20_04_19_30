99ff6297f30db65a044e68504fa47a73b894f958
Roman <kaboom.ua@gmail.com>
2017-04-27 06:12:06 +0300

    Update CommentList.js

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Comment from './Comment'
 
 export default function CommentList ({comments}) {
+  //comments может быть undefined
   const elements = comments.map(comment => <li key={comment.id}><Comment comment={comment} /></li>)
 
   return (
