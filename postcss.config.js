24e202bc842626b83d61b6669016fe67afc03a25
olegbilyk <oleg.bilik1994@gmail.com>
2017-04-30 16:08:30 +0300

    pull ht-2

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-short'),
-    require('postcss-cssnext')({browsers: ['last 3 versions', '> 5%']}),
-    require('stylelint'),
-    require('postcss-browser-reporter')()
-  ]
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,22 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const develop = process.env.NODE_ENV !== 'production';
+var path = require('path')
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
-
-  entry: {
-    app: './app.js'
-  },
-
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-
-  devtool: develop ? 'cheap-module-source-map' : false,
-
-  module: {
-    rules: [
-      // {
-      //   test: /\.js$/,
-      //   enforce: 'pre',
-      //   exclude: /node_modules/,
-      //   loader: 'eslint-loader'
-      // },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader'
+    devtool: 'source-map',
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loaders: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            }
         ]
-      },
-      {
-        test: /\.pcss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'postcss-loader'
-          ]
-        })
-      }
-    ]
-  },
-
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true,
-      disable: develop
-    })
-  ]
+    }
 }
