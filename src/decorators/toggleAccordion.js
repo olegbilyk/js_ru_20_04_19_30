381cfb8509163854d42093d3385488e91f43228f
Roman <kaboom.ua@gmail.com>
2017-05-10 19:10:42 +0300

    Update toggleAccordion.js

--- src/decorators/toggleAccordion.js
+++ src/decorators/toggleAccordion.js
@@ -1,7 +1,9 @@
 import React, { Component as BasicComponent } from 'react'
 
+//DecoratedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (OriginalComponent) => class DecorationComponent extends BasicComponent {
   state = {
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
     openAccordionId: null
   }
 
